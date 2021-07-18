7276b98aeb48de55750fc3ae744e45f2f088c5ca
Oleh Pysmenko <olehpysmenko@blessing.local>
2021-07-18 15:05:05 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length > 0) {
+    return Math.min(...array)
+} else {
+    return 0;
+}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+if (array && array.length > 0) {
+    return Math.max(...array)
+} else {
+    return 0;
 }
+}
+
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+  return ((array).reduce((a,v,i)=>(a*i+v)/(i+1))).toFixed(2);
+} else {
   return 0;
+}
 }
